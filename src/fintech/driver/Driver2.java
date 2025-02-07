25a758a99179ff8cb705779f9b3c34063ca686b8
andrey jonathan nathaniel nainggolan <143679440+andreyjonathann@users.noreply.github.com>
2025-02-07 16:42:27 +0700

    pair 20

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,44 @@
+
 package fintech.driver;
 
+import fintech.model.Account;
+import fintech.model.Transaction;
+import java.util.Scanner;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23017_Andrey Jonathan
+ * @author 12S23050_Yolanda Saragih
  */
 public class Driver2 {
 
     public static void main(String[] _args) {
+        Scanner input = new Scanner(System.in);
+        String perintah;
+        String accountName;
+        String testaccountName;
+        String owner;
+        String perintah2;
+        double amount=0.0;
+        String posted_at="";
+        String note="";
 
-        // codes
+
+        perintah = input.nextLine();
+        owner = input.nextLine();
+        accountName = input.nextLine();
+        perintah2 = input.nextLine();
+        testaccountName = input.nextLine();
+
+        if (accountName.equals(testaccountName)) {
+            amount = input.nextDouble();
+            input.nextLine();
+            posted_at = input.nextLine();
+            note = input.nextLine();
+        }
+
+        Transaction transactions = new Transaction(accountName, owner, amount, posted_at, note);
+        System.out.println(transactions.toString());
+        System.out.println(transactions.toString2());
 
     }
-
 }
--- src/fintech/model/Account.java
+++ src/fintech/model/Account.java
@@ -1,11 +1,26 @@
-package fintech.model;
+// File: src/fintech/model/Account.java
+ package fintech.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
+ /**
+ * @author 12S23017_Andrey Jonathan
+ * @author 12S23050_Yolanda Saragih
  */
-public class Account {
 
-    // class definition
-
-}
+ // <account-name>|<owner>|<balance>
+ public class Account {
+     // <account-name>|<owner>|<balance>
+     private String accountName;
+     private String owner;
+     private double balance = 0.0;
+ 
+     public Account(String accountName, String owner) {
+         this.accountName = accountName;
+         this.owner = owner;
+ 
+     }
+ 
+     public String toString() {
+         return owner + "|" + accountName + "|"  +balance;
+     }
+ 
+ }
